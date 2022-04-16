fad44c57145d1100e77f04213c505b7dc8d1551b
michael <cabreros17@gmail.com>
2022-04-15 23:24:16 -0400

    3.13 ui fix

--- phonebook/src/components/Persons.js
+++ phonebook/src/components/Persons.js
@@ -1,10 +1,15 @@
 const Persons = ({ filterNumbers, delPerson }) => {
   return (
     <>
+      console.log(person);
       {filterNumbers.map((person) => (
-        <li key={person.name}>
-          {person.name} {person.number}{" "}
-          <button onClick={delPerson} id={person.id} name={person.name}>
+        <li key={person.content.name}>
+          {person.content.name} {person.content.number}{" "}
+          <button
+            onClick={delPerson}
+            id={person.content.name}
+            name={person.content.name}
+          >
             delete
           </button>
         </li>
